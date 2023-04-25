c4986a2e69587972613bfea8d0dc7051ab293d1f
Hesures <128929471+Hesures@users.noreply.github.com>
2023-04-25 09:29:41 +0200

    Media queries and target menu
    CREAR UN AVISO, POR MENOR A 481PX, INDICAR QUE ESTA APP ES SOLO PARA INTERFACES
    MAYORES, TALES COMO IPADS, TABLETS, LAPTOPS Y DESKTOPS, NO DISPONIBLE PARA
    DISPOSITIVOS MÓVILES, Y HACER DESAPARECER TODO, DEJANDO VER SOLO EL MENSAJE

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,18 @@
+/* ------------- FOLLOW ME ---------------- */
+
 $(document).on("mousemove", function (event) {
     $("body").css("cursor", "none"); // para ocultar el cursor
-    $("#mainCharacter").css("left", event.pageX) // asocia la clase #mainCharacter a la posición del cursor
+    $("#mainCharacter").css("left", event.pageX+1) // asocia la clase #mainCharacter a la posición del cursor
                        .css("top", event.pageY);
 });
+
+/* ------------- PARCHMENTS ---------------- */
+
+var parchment = $(".parchment");
+var button = $(".parchmentButton");
+
+parchment.hide();
+
+$(".parchmentButton").on("click", () => {
+    parchment.slideToggle("medium");
+})
